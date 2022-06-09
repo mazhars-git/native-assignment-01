8de47d4e625e6e84f3108b245aa52ecff19c835e
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-06-09 08:45:10 +0600

    update

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,15 +1,32 @@
-import { View, Text, Pressable } from 'react-native'
+import { View, Text, Pressable, StyleSheet } from 'react-native'
 import React from 'react'
+import { colors } from '../theme/colors'
 
 export default function Home() {
   return (
-    <View>
+    <View style = {styles.container}>
       <Text>Covid-19</Text>
       <Pressable>
           <View>
-              
+
+              <Text>USA</Text>
           </View>
       </Pressable>
-    </View>
+
+      <View>
+          <Text>Are you feeling sick?</Text>
+          <Text>If you feel sick with any of covid-19 symptoms please call or SMS us immediately for help.</Text>
+      </View>
+
+    </View>    
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.blue,
+    height: 100,
+    color: colors.white,
+  }
+})
