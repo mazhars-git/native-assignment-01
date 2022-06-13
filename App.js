bdb0980bfe55b04baf988242b3834e2566bf3b34
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-06-13 10:48:54 +0600

    font added

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { colors } from './src/theme/colors';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/screens/home';
+import { useFonts } from 'expo-font';
+import Header from './src/components/header/header';
 
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  const [loaded] = useFonts({
+      'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+      'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
+      'Roboto-Reqular': require('./assets/fonts/Roboto-Regular.ttf'),
+  })
+
+  if (!loaded) {
+    <Text>Fonts Loading.............</Text>
+  }
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView>
+        <Header />
+        <Home />
+    </SafeAreaView>
   );
 }
 
