da9079bc5ad0e72d96d6f52e323336d53c3ed871
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-06-15 08:40:56 +0600

    design update

--- src/screens/home.js
+++ src/screens/home.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { colors } from '../theme/colors'
 import Text from '../components/text/text'
 import { spacing } from './../theme/spacing';
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
 
 export default function Home() {
   return (
@@ -12,16 +12,31 @@ export default function Home() {
         <Text preset="h1">Covid-19</Text>
         <Pressable>
             <View style= {styles.usaBtn}>
-                <Image style={{resizeMode: 'contain'}} source = {require("../../image/usa flag.png")} />
-                <Text>USA</Text>
+                <View>
+                <Image source = {require("../image/usa flag.png")} />
+                </View>
+
+                <Text style={{color: colors.black}}>USA</Text>
                 <AntDesign name="caretdown" size={10} color="black" />
             </View>
         </Pressable>
       </View>
-
+    
       <View>
           <Text>Are you feeling sick?</Text>
           <Text>If you feel sick with any of covid-19 symptoms please call or SMS us immediately for help.</Text>
+      </View>
+
+      <View>
+          <Pressable>
+              <Ionicons name="call" size={24} color= {colors.white} />
+              <Text>Call Now</Text>
+          </Pressable>
+
+          <Pressable>
+              <AntDesign name="message1" size={24} color={colors.white} />
+              <Text>Send SMS</Text>
+          </Pressable>
       </View>
 
     </View>    
@@ -44,6 +59,7 @@ const styles = StyleSheet.create({
 
   usaBtn: {
     flexDirection: 'row',
+    alignItems: 'center',
     justifyContent: 'space-between',
     backgroundColor: colors.white,
     padding: spacing[4],
